a9d7f7a0885c3d57b4d12ad14607c51846f18640
Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>
2015-04-21 20:15:12 -0300

    Updated demo with the new framework

--- init.lua
+++ init.lua
@@ -1,19 +1,19 @@
-local framework = require('framework')
-local PollingPlugin = framework.PollingPlugin
+local framework = require('./modules/framework')
+local Plugin = framework.Plugin
 local RandomDataSource = framework.RandomDataSource
 
 local params = framework.params
-params.name = 'LUA demo plugin'
+params.name = 'Boundary Demo Plugin'
 params.version = '1.0'
 params.minValue = params.minValue or 1
 params.maxValue = params.maxValue or 100
 
-local dataSource = RandomDataSource:new(params.minValue, params.maxValue)
-local plugin = PollingPlugin:new(params, dataSource)
-function plugin:onParseValues(data)
-	local result = {}
-	result['BOUNDARY_LUA_SAMPLE'] = tonumber(data)
+local data_source = RandomDataSource:new(params.minValue, params.maxValue)
+local plugin = Plugin:new(params, data_source)
 
+function plugin:onParseValues(val)
+	local result = {}
+	result['BOUNDARY_SAMPLE_METRIC'] = val
 	return result 
 end
 
